d2346a16fd01654c773b13cb194a86bfa17d54ef
manavgathani <manavgathani@gmail.com>
2022-09-01 10:26:02 +0530

    Module 3

--- Module 3/count number of strings in list.py
+++ Module 3/count number of strings in list.py
@@ -1,11 +1,11 @@
 # a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings.
 
-mylist=[]
-n=int(input("Enter number of list : "))
+def match_words(words):
+    ctr=0
 
-for n in range(n):
-    value=(input("Enter values : "))
+    for word in words:
+        if len(word)>1 and word[0]==word[-1]:
+            ctr+=1
+    return ctr
 
-    if len(value) >= 2:
-        print()
-    
+print(match_words(["abc","xyz","aba","1221"]))
